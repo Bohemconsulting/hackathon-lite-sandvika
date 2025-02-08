49cf1daa950a87fc46ce339c1b15091b1b1a7dbe
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-08 13:28:06 +0100

    feat: run next-image-experimental codemod

--- src/components/sections/summary-section.tsx
+++ src/components/sections/summary-section.tsx
@@ -1,5 +1,5 @@
 import { Clock, Laptop, MapPin, User } from "lucide-react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export function SummarySection() {
   return (
