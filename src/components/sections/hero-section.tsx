a98c788595d83ef2496040ba69c13033871a4761
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-08 13:39:08 +0100

    feat: change from button to link

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 
 export function HeroSection() {
   return (
-    (<div className="relative h-[266px]">
+    <div className="relative h-[266px]">
       <div className="w-full">
         <Image
           src="/hero-cover.png"
@@ -12,21 +14,25 @@ export function HeroSection() {
           fill
           sizes="100vw"
           style={{
-            objectFit: "cover"
-          }} />
+            objectFit: "cover",
+          }}
+        />
       </div>
       <div className="absolute flex h-full w-full flex-col items-center">
         <div className="h-[124px]" />
         <p className="size-28 h-[44px] w-[353px] text-center font-bold text-white">
-          Bli med på vårt Hackathon i Sandvika 28. februar - 28.mars
+          Bli med på vårt Hackathon i Sandvika 28. februar - 4. april
         </p>
-        <Button className="mt-[22px] h-[40px] w-[140px]">
+        <Link
+          href="#sign-up-form"
+          className={cn(buttonVariants({ variant: "default" }), "mt-[22px]")}
+        >
           Gå til påmelding
-        </Button>
+        </Link>
         <p className="mt-[9px] size-2 h-[15px] w-[250px] text-gray-400 italic">
           *psst* vi har premier på 25 000 kr +
         </p>
       </div>
-    </div>)
+    </div>
   );
 }
