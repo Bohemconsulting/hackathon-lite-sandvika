1dcf34c2355a125e34b3b31f3f7d4d3153a3a31e
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-08 13:25:44 +0100

    feat: add container to navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -15,30 +15,32 @@ const links = [
 
 export function Navbar() {
   return (
-    <nav className="flex flex-row items-center justify-between bg-gray-800 p-4">
-      <div className="flex flex-row items-center text-xl">
-        <Image
-          src="/logos/hls-logo.png"
-          width={18}
-          height={18}
-          alt="Hackathon Lite Sandvika logo"
-          className="mx-1"
-        />
-        <h1>
-          <span className="text-yellow uppercase">Hackathon</span>
-          <span className="text-turquoise uppercase">Lite</span>
-        </h1>
-      </div>
-      <div className="flex flex-row divide-x-2">
-        {links.map((link) => (
-          <Link
-            key={link.text}
-            href={link.href}
-            className="border-foreground px-2"
-          >
-            {link.text}
-          </Link>
-        ))}
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex flex-row items-center justify-between px-2">
+        <div className="flex flex-row items-center text-xl">
+          <Image
+            src="/logos/hls-logo.png"
+            width={18}
+            height={18}
+            alt="Hackathon Lite Sandvika logo"
+            className="mx-1"
+          />
+          <h1>
+            <span className="text-yellow uppercase">Hackathon</span>
+            <span className="text-turquoise uppercase">Lite</span>
+          </h1>
+        </div>
+        <div className="flex flex-row divide-x-2">
+          {links.map((link) => (
+            <Link
+              key={link.text}
+              href={link.href}
+              className="border-foreground px-2"
+            >
+              {link.text}
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   );
