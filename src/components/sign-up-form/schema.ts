93dcdb327b98db33c5008b3662e8b3ebafd05bf8
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-12 00:43:02 +0100

    feat: update schema error messages

--- src/components/sign-up-form/schema.ts
+++ src/components/sign-up-form/schema.ts
@@ -5,16 +5,17 @@ export const formSchema = z.object({
   email: z.string().email("Ugyldig email"),
   phoneNumber: z.string().regex(/^[49][0-9]{7}$/, "Ugyldig telefonnummer"),
   age: z.string().regex(/^\d+$/, "Alder må være et tall"),
-  gender: z.enum(["male", "female", "other"]),
+  gender: z.enum(["male", "female", "other"], {
+    required_error: "Du må velge et kjønn",
+  }),
   postalCode: z.string().regex(/^\d+$/, "Postnummer må være et tall"),
-  programmingCompetence: z.enum(["beginner", "experienced"]),
-  hoodieSize: z.enum([
-    "small",
-    "medium",
-    "large",
-    "extra_large",
-    "extra_extra_large",
-  ]),
+  programmingCompetence: z.enum(["beginner", "experienced"], {
+    required_error: "Du må velge et programmeringsnivå",
+  }),
+  hoodieSize: z.enum(
+    ["small", "medium", "large", "extra_large", "extra_extra_large"],
+    { required_error: "Du må velge en hettegenserstørrrelse" },
+  ),
 });
 
 export type FormSchema = z.infer<typeof formSchema>;
