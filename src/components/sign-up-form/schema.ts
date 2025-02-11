de0ef1b1e11356b7b4e71929ab0110095067e5df
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-12 00:29:29 +0100

    feat: update form schema

--- src/components/sign-up-form/schema.ts
+++ src/components/sign-up-form/schema.ts
@@ -4,6 +4,17 @@ export const formSchema = z.object({
   name: z.string().min(1, "Et navn må minst inneholde ett tegn"),
   email: z.string().email("Ugyldig email"),
   phoneNumber: z.string().regex(/^[49][0-9]{7}$/, "Ugyldig telefonnummer"),
+  age: z.number().positive("Alder må være et positivt tall"),
+  gender: z.enum(["male", "female", "other"]),
+  postalCode: z.number().positive("Postnummer må være positivt"),
+  programmingCompetence: z.enum(["beginner", "experienced"]),
+  hoodieSize: z.enum([
+    "small",
+    "medium",
+    "large",
+    "extra_large",
+    "extra_extra_large",
+  ]),
 });
 
 export type FormSchema = z.infer<typeof formSchema>;
