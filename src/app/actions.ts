b17980e32abe688e5d60b380726c7027997b0b63
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-12 01:08:52 +0100

    Feature/update db (#4)
    * feat: update form schema
    
    * feat: add shadcn radio-group
    
    * feat: gen db types
    
    * feat: update schema
    
    * feat: update schema error messages
    
    * feat: update form ui and backend action
    
    * feat: remove console logs

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,31 @@
 "use server";
 
-import { formSchema as eventSignupSchema } from "@/components/sign-up-form/schema";
 import { TablesInsert } from "@/generated/database.types";
 import { createClient } from "@/lib/supabase/server";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  phone_number: z.string().regex(/^[49][0-9]{7}$/),
+  age: z.number(),
+  gender: z.enum(["male", "female", "other"]),
+  postal_code: z.number(),
+  programming_competence: z.enum(["beginner", "experienced"]),
+  hoodie_size: z.enum([
+    "small",
+    "medium",
+    "large",
+    "extra_large",
+    "extra_extra_large",
+  ]),
+});
 
 export async function createEventSignup(
   eventSignup: TablesInsert<"EventSignup">,
 ) {
-  const result = eventSignupSchema.safeParse(eventSignup);
+  const result = formSchema.safeParse(eventSignup);
+
   if (result.error) {
     return;
   }
--- src/components/sign-up-form/index.tsx
+++ src/components/sign-up-form/index.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { useEventSignupMutation } from "./query";
 
 export function SignUpForm() {
@@ -25,6 +26,11 @@ export function SignUpForm() {
       name: "",
       email: "",
       phoneNumber: "",
+      age: "0",
+      gender: undefined,
+      postalCode: "0",
+      programmingCompetence: undefined,
+      hoodieSize: undefined,
     },
   });
 
@@ -33,6 +39,11 @@ export function SignUpForm() {
       name: values.name,
       email: values.email,
       phone_number: values.phoneNumber,
+      age: parseInt(values.age),
+      gender: values.gender,
+      postal_code: parseInt(values.postalCode),
+      programming_competence: values.programmingCompetence,
+      hoodie_size: values.hoodieSize,
     });
 
     if (result?.error != null) {
@@ -96,6 +107,151 @@ export function SignUpForm() {
                 <FormLabel>Telefonnummer</FormLabel>
                 <FormControl>
                   <Input placeholder="XXXXXXXX" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Alder</FormLabel>
+                <FormControl>
+                  <Input type="number" min={0} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Kjønn</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="male" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Mann</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="female" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Kvinne</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="other" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Annet</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="postalCode"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Postnummer</FormLabel>
+                <FormControl>
+                  <Input type="number" min={0} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="programmingCompetence"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Programmeringsnivå</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="beginner" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Nybegynner</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="experienced" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Erfaren</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="hoodieSize"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Hettegenserstørrelse</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="small" />
+                      </FormControl>
+                      <FormLabel className="font-normal">S</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="medium" />
+                      </FormControl>
+                      <FormLabel className="font-normal">M</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="large" />
+                      </FormControl>
+                      <FormLabel className="font-normal">L</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="extra_large" />
+                      </FormControl>
+                      <FormLabel className="font-normal">XL</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-y-0 space-x-3">
+                      <FormControl>
+                        <RadioGroupItem value="extra_extra_large" />
+                      </FormControl>
+                      <FormLabel className="font-normal">XXL</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
                 </FormControl>
                 <FormMessage />
               </FormItem>
