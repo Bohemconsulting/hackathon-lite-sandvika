9478c79ae2e846b45e80cf9990dcfd6691ee12ae
DanielGrotan <78237277+DanielGrotan@users.noreply.github.com>
2025-02-19 20:01:48 +0100

    feat: remove signup form and replace it with other info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { HeroSection } from "@/components/sections/hero-section";
 import { HostSection } from "@/components/sections/host-section";
 import { InfoSection } from "@/components/sections/info-section";
 import { PrizesSection } from "@/components/sections/prizes-section";
+import { SignupClosedSection } from "@/components/sections/signup-closed-section";
 import { SponsorsSection } from "@/components/sections/sponsors-section";
 import { SummarySection } from "@/components/sections/summary-section";
-import { SignUpForm } from "@/components/sign-up-form";
 
 const navbarLinks = [
   {
@@ -30,7 +30,8 @@ export default function Home() {
           <HostSection />
           <InfoSection />
           <PrizesSection />
-          <SignUpForm />
+          {/* <SignUpForm /> Sign Up is closed */}
+          <SignupClosedSection />
           <SponsorsSection />
         </div>
       </div>
